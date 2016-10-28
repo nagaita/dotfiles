0dfcd90f2b962837410346d62d3af346a2a32a58
Takuma Nagai <nagai_ta@worksap.co.jp>
2016-10-28 12:06:20 +0900

    add script for put dotfiles to home directory

---
+++ syncdot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -euvx
+
+ln -sfv $(pwd)/dot.Xmodmap $HOME/.Xmodmap
+ln -sfv $(pwd)/dot.peco $HOME/.peco
+ln -sfv $(pwd)/dot.tmux.conf $HOME/.tmux.conf
+ln -sfv $(pwd)/dot.vim $HOME/.vim
+ln -sfv $(pwd)/dot.vimrc $HOME/.vimrc
+ln -sfv $(pwd)/dot.zshrc $HOME/.zshrc
+ln -sfv $(pwd)/dot.zshenv $HOME/.zshenv
+ln -sfv $(pwd)/dot.emacs.d $HOME/.emacs.d
