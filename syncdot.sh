1f2c4a0c7f09b9957017909e9da48b134457c278
Takuma Nagai <tn00gm@gmail.com>
2017-09-23 11:25:36 +0900

    Add .bash_profile.

--- syncdot.sh
+++ syncdot.sh
@@ -1,11 +1,15 @@
-#!/bin/bash -euvx
+#!/bin/bash
 
-ln -sfv $(pwd)/dot.Xmodmap $HOME/.Xmodmap
-ln -sfv $(pwd)/dot.peco $HOME/.peco
-ln -sfv $(pwd)/dot.tmux.conf $HOME/.tmux.conf
-ln -sfv $(pwd)/dot.vim $HOME/.vim
-ln -sfv $(pwd)/dot.vimrc $HOME/.vimrc
-ln -sfv $(pwd)/dot.zshrc $HOME/.zshrc
-ln -sfv $(pwd)/dot.zshenv $HOME/.zshenv
-ln -sfv $(pwd)/dot.emacs.d $HOME/.emacs.d
-ln -sfv $(pwd)/config.fish $HOME/.config/fish/
+set -eu
+
+ln -sfv "$(pwd)/dot.Xmodmap" "$HOME/.Xmodmap"
+ln -sfv "$(pwd)/dot.peco" "$HOME/.peco"
+ln -sfv "$(pwd)/dot.tmux.conf" "$HOME/.tmux.conf"
+ln -sfv "$(pwd)/dot.vim" "$HOME/.vim"
+ln -sfv "$(pwd)/dot.vimrc" "$HOME/.vimrc"
+ln -sfv "$(pwd)/dot.zshrc" "$HOME/.zshrc"
+ln -sfv "$(pwd)/dot.zshenv" "$HOME/.zshenv"
+ln -sfv "$(pwd)/dot.emacs.d" "$HOME/.emacs.d"
+ln -sfv "$(pwd)/config.fish" "$HOME/.config/fish/"
+ln -sfv "$(pwd)/dot.gitconfig" "$HOME/.gitconfig"
+ln -sfv "$(pwd)/dot.bash_profile" "$HOME/.bash_profile"
